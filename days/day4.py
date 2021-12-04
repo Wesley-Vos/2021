41145838b20c2226c4043b22426e924f5d902a51
Wesley Vos <Wesley-Vos@users.noreply.github.com>
2021-12-04 09:31:35 +0100

    Improved day4

--- days/day4.py
+++ days/day4.py
@@ -11,20 +11,18 @@ class Day4(Day):
         super().__init__(filename)
         self.bingo = Bingo(self.data)
         
-
     def solve_part1(self):
         return self.bingo.play(True)
 
     def solve_part2(self):
         return self.bingo.play(False)
 
+
 class Bingo:
     class Card:
         def __init__(self, data):
             self._data = [list(map(int, d.split())) for d in data]
             self.won = False
-            #print(self._data)
-            #self._remain = 25
     
         def check(self, draw):
             if self.won:
@@ -32,11 +30,8 @@ class Bingo:
             
             for i, row in enumerate(self._data):
                 for j, num in enumerate(row):
-                    #print(num, draw)
                     if num == draw:
-                        #print("found")
-                        self._data[i][j] = -1
-                        #self._remain -= 1
+                        self._data[i][j] = 0
                         hor = len(set(self._data[i])) == 1
                         vert = len(set([self._data[idx][j] for idx in range(0, 5)])) == 1
                         if vert or hor:
@@ -45,27 +40,24 @@ class Bingo:
             return False
             
         def sum_unmarked(self):
-            sum_val = 0
-            for row in self._data:
-                sum_val += sum(n for n in row if n != -1)
-            return sum_val
+            return sum(sum(n for n in row) for row in self._data)
 
     def __init__(self, data):
         data = [d for d in data if d != '']
-        self.numbers = list(map(int, data[0].split(",")))
-        self.cards = [self.Card(data[i:i+5]) for i in range(1, len(data), 5)]
+        self._numbers = [int(d) for d in data[0].split(",")]
+        self._cards = [self.Card(data[i:i+5]) for i in range(1, len(data), 5)]
 
     def play(self, first):
         last_val = 0
-        for draw in self.numbers:
-            for card in self.cards:
+        for draw in self._numbers:
+            for card in self._cards:
                 if card.check(draw):
                     if first:
                         return card.sum_unmarked()*draw
                     else:
                         last_val = card.sum_unmarked()*draw
         return last_val
-        
+
 
 def main():
     day = Day4("day4.txt")
