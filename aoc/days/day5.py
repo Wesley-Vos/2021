2d0a8c459c4e5cb2608dbdbfa74748d0afd91346
Wesley Vos <wesley.vos@icloud.com>
2021-12-05 18:39:27 +0100

    Improved version day5

--- aoc/days/day5.py
+++ aoc/days/day5.py
@@ -1,120 +1,48 @@
 from aoc.util.Day import Day
+import re
 
 
-class HTV:
-    class Line:
-        def __init__(self, start, end):
-            self.start = start
-            self.end = end 
-    
-    
+class HydroThermalVents:
     def __init__(self, data):
-        self.hor = {}
-        self.vert = {}
-        self.diag = []
-        self.maxx = 0
-        self.maxy = 0
-        for row in data:
-            start, end = row.split(" -> ")
-            x1, y1 = map(int, start.split(","))
-            x2, y2 = map(int, end.split(","))
-            self.maxx = max(self.maxx, x2)
-            self.maxy = max(self.maxy, y2)
-            if x1 == x2:    #vert
-                y1, y2 = sorted((y1, y2))
-                if x1 in self.vert:
-                    self.vert[x1].append((y1, y2))
-                else:
-                    self.vert[x1] = [(y1, y2)]
-            elif y1 == y2:    #horiz
-                x1, x2 = sorted((x1, x2))
-                if y1 in self.hor:
-                    self.hor[y1].append((x1, x2))
-                else:
-                    self.hor[y1] = [(x1, x2)]
-            elif abs(x1 - x2) == abs(y1 - y2):
-                # (x1 == y1 and x2 == y2) or (x1 == y2 and x2 == y1):
-                #(x1, y1), (x2, y2) = sorted(((x1, y1), (x2, y2)))
-                self.diag.append(((x1, y1), (x2, y2)))
-            else:
-                pass
-        
-    def cnt(self):
-        start = (0, 0)
-        maxx = 0
-        maxy = 0
-           
-        end = (self.maxx, self.maxy)
-        print(end)
-        #return 0
-        
-        data = [[0 for _ in range(0, end[0] + 2)] for _ in range(0, end[1] + 2)]
+        self.visits = {}
+        self.data = data
 
-        #print("\n".join(map(lambda r: " ".join(map(str, r)), data)))
-        
-        for idx, row in self.hor.items():
-            for x1, x2 in row:
-                for i in range(x1, x2 + 1):
-                    data[idx][i] += 1
-                    
-                    
-        for idx, row in self.vert.items():
-            for y1, y2 in row:
-                for i in range(y1, y2 + 1):
-                    data[i][idx] += 1
-                    
-        for row in self.diag:
-            #print(row)
-            if row[0][0] == 1 + row[0][1]:
-                for i in range(row[0][0], row[1][0] + 1):
-                    #print(i, i)
-                    data[i][i] += 1
-            else:
-                row = sorted(row)
-                #print(row)
-                (x1, y1), (x2, y2) = row
+    def check(self, straight_only=True):
+        reg_pattern = r"(\d*),(\d*) -> (\d*),(\d*)"
+        for row in self.data:
+            match = re.findall(reg_pattern, row, re.MULTILINE)[0]
+            x1, y1, x2, y2 = map(int, match)
+
+            if x1 == x2:
+                y_range = range(y1, y2 + 1) if y2 > y1 else range(y2, y1 + 1)
+                for y in y_range:
+                    self.visits[(x1, y)] = 1 + self.visits.get((x1, y), 0)
+            elif y1 == y2:
+                x_range = range(x1, x2 + 1) if x2 > x1 else range(x2, x1 + 1)
+                for x in x_range:
+                    self.visits[(x, y1)] = 1 + self.visits.get((x, y1), 0)
+            elif not straight_only and abs(x1 - x2) == abs(y1 - y2):
                 amount = abs(x1 - x2)
-                
-                if x2 > x1:
-                    xran = list(range(x1, x2 + 1))
-                else:
-                    xran = list(range(x2, x1 + 1))
-                    xran.sort(reverse=True)
+                x_range = list(range(x1, x2 + 1)) if x2 > x1 else sorted(list(range(x2, x1 + 1)), reverse=True)
+                y_range = list(range(y1, y2 + 1)) if y2 > y1 else sorted(list(range(y2, y1 + 1)), reverse=True)
 
-                #print(y1, y2)
-                if y2 > y1:
-                    yran = list(range(y1, y2 + 1))
-                else:
-                    #print(list(range(y2, y1 + 1)))
-                    yran = list(range(y2, y1 + 1))
-                    yran.sort(reverse = True)
-                    
-                #print(xran)
-                #print(yran)
-                    
                 for i in range(0, amount + 1):
-                    #print(xran[i], yran[i])
-                    data[yran[i]][xran[i]] += 1
-        
-        res = 0
-        for row in data:
-            res += len(list(filter(lambda c: c >= 2, row)))
-        
-        
-        return res
+                    x, y = x_range[i], y_range[i]
+                    self.visits[(x, y)] = 1 + self.visits.get((x, y), 0)
+
+        return len([cnt for cnt in self.visits.values() if cnt >= 2])
 
 
 class Day5(Day):
-
     def __init__(self, filename):
         super().__init__(filename)
+        self.htv = HydroThermalVents(self.data)
         
     def solve_part1(self):
-        htv = HTV(self.data)
-        return htv.cnt()
+        return self.htv.check()
 
     def solve_part2(self):
-        return 0
+        return self.htv.check(False)
 
 
 def main():
--- tests/day5.py
+++ tests/day5.py
@@ -5,22 +5,22 @@ from aoc.days.day5 import Day5
 
 class Day3Test(unittest.TestCase):
     def test_example_1(self):
-        example_answer1 = 4512
+        example_answer1 = 5
         day = Day5("../input/test/day5.in")
         self.assertEqual(day.solve_part1(), example_answer1, f"The example test for part 1 should return {example_answer1}")
 
     def test_example_2(self):
-        example_answer2 = 1924
+        example_answer2 = 12
         day = Day5("../input/test/day5.in")
         self.assertEqual(day.solve_part2(), example_answer2, f"The example test for part 2 should return {example_answer2}")
 
     def test_part1(self):
-        answer1 = 32844
+        answer1 = 6113
         day = Day5("../input/day5.in")
         self.assertEqual(day.solve_part1(), answer1, f"The test for part 1 should return {answer1}")
 
     def test_part2(self):
-        answer2 = 4920
+        answer2 = 20373
         day = Day5("../input/day5.in")
         self.assertEqual(day.solve_part2(), answer2, f"The test for part 2 should return {answer2}")
 
