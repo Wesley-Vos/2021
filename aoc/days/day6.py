f714c004b5bac51fadb41598d0f38e6275c7186b
Wesley Vos <wesley.vos@icloud.com>
2021-12-06 10:38:18 +0100

    Code refactor

--- aoc/days/day6.py
+++ aoc/days/day6.py
@@ -3,19 +3,18 @@ from aoc.util.Day import Day
 
 class LanternFishGrow:
     def __init__(self, data):
+        self.days = None
         self.data = data
         self._set_initial_data()
 
     def _set_initial_data(self):
+        self.days = 1
         self.population = [0 for i in range(0, 9)]
         for fish in map(int, self.data[0].split(",")):
             self.population[fish] += 1
 
     def __str__(self):
         return '\t'.join(map(lambda f: str(f[0]) + ": " + str(f[1]), self.population))
-
-    def reset(self):
-        self._set_initial_data()
 
     def _age(self):
         init_day0 = self.population[0]
@@ -29,8 +28,12 @@ class LanternFishGrow:
         self.population[6] += init_day0
 
     def age(self, days):
-        for _ in range(days):
+        if self.days < days:
+            self._set_initial_data()
+
+        while self.days <= days:
             self._age()
+            self.days += 1
 
     def size(self):
         return sum(self.population)
@@ -46,7 +49,6 @@ class Day6(Day):
         return self.grow.size()
 
     def solve_part2(self):
-        self.grow.reset()
         self.grow.age(256)
         return self.grow.size()
 
