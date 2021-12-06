f729cc6600745d529de9b87907d2b4ee877be52f
Wesley Vos <wesley.vos@icloud.com>
2021-12-06 07:55:32 +0100

    Finished day6

---
+++ aoc/days/day6.py
@@ -0,0 +1,71 @@
+from aoc.util.Day import Day
+import re
+
+
+class LanternFishGrow:
+    class Lantern:
+        def __init__(self, initial_state=None):
+            self.timer = initial_state or 8
+
+        def age(self):
+            self.timer -= 1
+
+        def __str__(self):
+            return str(self.timer)
+
+    def __init__(self, data):
+        self.data = data
+        self._set_initial_data()
+
+    def _set_initial_data(self):
+        self.population = {day: 0 for day in range(0, 9)}
+        for fish in map(int, self.data[0].split(",")):
+            self.population[fish] += 1
+
+    def __str__(self):
+        return '\t'.join(map(lambda f: str(f[0]) + ": " + str(f[1]), self.population.items()))
+
+    def reset(self):
+        self._set_initial_data()
+
+    def _age(self):
+        init_day0 = self.population[0]
+        for day in range(0, 9):
+            if day == 0:
+                self.population[day] = 0
+            else:  # (day > 0)
+                self.population[day - 1] += self.population[day]
+                self.population[day] = 0
+        self.population[8] += init_day0  # new population
+        self.population[6] += init_day0
+
+    def age(self, days):
+        for _ in range(days):
+            self._age()
+
+    def size(self):
+        return sum(self.population.values())
+
+
+class Day6(Day):
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.grow = LanternFishGrow(self.data)
+        
+    def solve_part1(self):
+        self.grow.age(80)
+        return self.grow.size()
+
+    def solve_part2(self):
+        self.grow.reset()
+        self.grow.age(256)
+        return self.grow.size()
+
+
+def main():
+    day = Day6("day6.in")
+    day.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/day6.py
@@ -0,0 +1,29 @@
+import unittest
+
+from aoc.days.day6 import Day6
+
+
+class Day3Test(unittest.TestCase):
+    def test_example_1(self):
+        example_answer1 = 5934
+        day = Day6("../input/test/day6.in")
+        self.assertEqual(day.solve_part1(), example_answer1, f"The example test for part 1 should return {example_answer1}")
+
+    def test_example_2(self):
+        example_answer2 = 26984457539
+        day = Day6("../input/test/day6.in")
+        self.assertEqual(day.solve_part2(), example_answer2, f"The example test for part 2 should return {example_answer2}")
+
+    def test_part1(self):
+        answer1 = 374927
+        day = Day6("../input/day6.in")
+        self.assertEqual(day.solve_part1(), answer1, f"The test for part 1 should return {answer1}")
+
+    def test_part2(self):
+        answer2 = 20373
+        day = Day6("../input/day6.in")
+        self.assertEqual(day.solve_part2(), answer2, f"The test for part 2 should return {answer2}")
+
+
+if __name__ == '__main__':
+    unittest.main()
