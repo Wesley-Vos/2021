965700c1c082a7142787808ab0851b5a550cdaad
Wesley Vos <wesley.vos@icloud.com>
2021-12-13 10:56:24 +0100

    Improved version day13

--- aoc/days/day13.py
+++ aoc/days/day13.py
@@ -1,74 +1,86 @@
 from aoc.util.Day import Day
-from collections import defaultdict
 import re
 
 
 class Paper:
     def __init__(self, data):
-        self.grid = defaultdict(lambda: 0)
+        self.grid = {}
         self.size = {"x": 0, "y": 0}
         self.fold_instructions = []
-        for row in filter(lambda r: r != "", data):
-            if row[0].isnumeric():
-                x, y = map(int, row.split(","))
-                self.size["x"] = max(self.size["x"], x + 1)
-                self.size["y"] = max(self.size["y"], y + 1)
-                self.grid[(x, y)] = 1
-            else:
-                reg_pattern = r"fold along ([xy])=(\d*)"
-                xy, val = re.findall(reg_pattern, row, re.MULTILINE)[0]
-                self.fold_instructions.append({'x': int(val) if xy == 'x' else None, 'y': int(val) if xy == 'y' else None})
+        for x, y in data:
+            self.size["x"] = max(self.size["x"], x + 1)
+            self.size["y"] = max(self.size["y"], y + 1)
+            self.grid[(x, y)] = 1
 
     def __str__(self):
-        out = ""
-        for y in range(self.size['y']):
-            out += f"{y}: "
-            for x in range(self.size['x']):
-                out += "  " if not self.grid[(x, y)] else "##"
-            out += "\n"
-        return out
+        return "\n".join("".join("██" if (x, y) in self else "  " for x in range(self.size['x'])) for y in range(self.size['y']))
 
-    def fold(self, x_fold, y_fold):
-        print(f"Fold along x={x_fold}, y={y_fold}, size={self.size}")
-        if x_fold is not None:
-            dx = x_fold
-            for y in range(self.size['y']):
-                for x in range(dx, self.size['x']):
-                    opp = (self.size['x'] - x - 1, y)
-                    #print((x, y), opp)
-                    self.grid[opp] = self.grid[(x, y)] or self.grid[opp]
-                    self.grid.pop((x, y), None)
-            self.size['x'] = x_fold
+    def __contains__(self, item):
+        return item in self.grid
 
-        if y_fold is not None:
-            dy = y_fold
-            for y in range(dy, self.size['y']):
-                for x in range(self.size['x']):
-                    opp = (x, self.size['y'] - y - 1)
-                    #print((x, y), opp)
-                    self.grid[opp] = self.grid[(x, y)] or self.grid[opp]
-                    self.grid[(x, y)] = 0
-            self.size['y'] = y_fold
+    def set(self, pos, val):
+        self.grid[pos] = val
+
+    def remove(self, pos):
+        self.grid.pop(pos, None)
 
     def cnt_dots(self):
-        return sum(1 for val in self.grid.values() if val)
+        return len(self.grid.values())
+
+
+class PaperFolder:
+    def __init__(self, paper, instructions):
+        self.paper = paper
+        self.instructions = instructions
+
+    def fold_all(self):
+        while len(self.instructions) > 0:
+            self.fold()
+
+    def fold(self):
+        x_fold, y_fold = self.instructions.pop(0)
+        dx = (x_fold + 1) if x_fold is not None else 0
+        dy = (y_fold + 1) if y_fold is not None else 0
+        size = {'x': x_fold or self.paper.size['x'], 'y': y_fold or self.paper.size['y']}
+
+        for y in range(dy, self.paper.size['y']):
+            for x in range(dx, self.paper.size['x']):
+                opp = (2*x_fold - x, y) if x_fold is not None else (x, 2*y_fold - y)
+                if opp in self.paper or (x, y) in self.paper:
+                    self.paper.set(opp, 1)
+                self.paper.remove((x, y))
+
+        self.paper.size = size
 
 
 class Day13(Day):
     def __init__(self, filename):
         super().__init__(filename)
-        self.paper = Paper(self.data)
+        grid, instructions = self._split_input()
+        self.paper = Paper(grid)
+        self.folder = PaperFolder(self.paper, instructions)
+
+    def _split_input(self):
+        grid, instructions = [], []
+
+        for row in filter(lambda r: r != "", self.data):
+            if row[0].isnumeric():
+                grid.append(tuple(map(int, row.split(","))))
+            else:
+                reg_pattern = r"fold along ([xy])=(\d*)"
+                xy, val = re.findall(reg_pattern, row, re.MULTILINE)[0]
+                instructions.append((int(val) if xy == 'x' else None, int(val) if xy == 'y' else None))
+
+        return grid, instructions
 
     def solve_part1(self):
-        fold_instr = self.paper.fold_instructions[0]
-        self.paper.fold(x_fold=fold_instr["x"], y_fold=fold_instr["y"])
+        self.folder.fold()
         return self.paper.cnt_dots()
 
     def solve_part2(self):
-        for fold_instr in self.paper.fold_instructions[1:]:
-            self.paper.fold(x_fold=fold_instr["x"], y_fold=fold_instr["y"])
+        self.folder.fold_all()
         print(self.paper)
-        return "CJHXZKRU"
+        return "CJHAZHKU"
 
 
 def main():
