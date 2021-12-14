070e51835c18e85ccbeef9809288cde14bf4eb0d
Wesley Vos <wesley.vos@icloud.com>
2021-12-14 11:24:41 +0100

    Improved day13

--- aoc/days/day13.py
+++ aoc/days/day13.py
@@ -5,13 +5,13 @@ from aoc.util.Day import Day
 
 class Paper:
     def __init__(self, data):
-        self.grid = {}
+        self.grid = set()
         self.size = {"x": 0, "y": 0}
         self.fold_instructions = []
         for x, y in data:
             self.size["x"] = max(self.size["x"], x + 1)
             self.size["y"] = max(self.size["y"], y + 1)
-            self.grid[(x, y)] = 1
+            self.grid.add((x, y))
 
     def __str__(self):
         return "\n".join("".join("██" if (x, y) in self.grid else "  " for x in range(self.size['x'])) for y in
@@ -19,13 +19,13 @@ class Paper:
 
     def mirror(self, des, src):
         if src in self.grid or des in self.grid:
-            self.grid[des] = 1
+            self.grid.add(des)
 
     def remove(self, pos):
-        self.grid.pop(pos, None)
+        self.grid.discard(pos)
 
     def cnt_dots(self):
-        return len(self.grid.values())
+        return len(self.grid)
 
 
 class PaperFolder:
@@ -45,8 +45,8 @@ class PaperFolder:
         dx, dy = ((x_fold or -1) + 1), ((y_fold or -1) + 1)
         size = {'x': x_fold or self.paper.size['x'], 'y': y_fold or self.paper.size['y']}
 
-        for x in range(dx, self.paper.size['x']):
-            for y in range(dy, self.paper.size['y']):
+        for (x, y) in set(self.paper.grid):
+            if x in range(dx, self.paper.size['x']) and y in range(dy, self.paper.size['y']):
                 mirrored = (2 * x_fold - x, y) if x_fold is not None else (x, 2 * y_fold - y)
                 self.paper.mirror(mirrored, (x, y))
                 self.paper.remove((x, y))
