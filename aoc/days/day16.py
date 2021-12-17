a4c0d59e505467b1eec45f41aef18e7defebff42
Wesley Vos <wesley.vos@icloud.com>
2021-12-17 12:09:41 +0100

    Finished day16

--- aoc/days/day16.py
+++ aoc/days/day16.py
@@ -1,7 +1,6 @@
 from aoc.util.Day import Day
-import sys
+from math import prod
 
-sys.setrecursionlimit(50)
 
 class PacketDecoder:
     def __init__(self, data):
@@ -13,10 +12,8 @@ class PacketDecoder:
         return bin(int(string, 16))[2:].zfill(len(string) * 4)
 
     def _parse(self, packet):
-        #print(f"Packet to decode {packet}")
         version = int(packet[0:3], 2)
         type_ID = int(packet[3:6], 2)
-        #print(f"Version: {version} and type: {type_ID}")
         i = 6
         new_packet = None
         if type_ID == 4:
@@ -30,34 +27,27 @@ class PacketDecoder:
                 i += 4
             value = int(bit_string, 2)
             new_packet = Literal(version, type_ID, value)
-
         else:
             length_type_ID = int(packet[i], 2)
             subs = []
             i += 1
             if length_type_ID == 0:
                 length_subpackets = int(packet[i:i + 15], 2)
-                #print(f"Length mode, {length_subpackets}")
                 i += 15
                 len_after = len(packet[i:]) - length_subpackets
                 while len(packet) > len_after:
                     new_packet, packet = self._parse(packet[i:])
                     subs.append(new_packet)
                     i = 0
-                    #print(f"Got: {new_packet}")
-
             else:
                 numb_subpackets = int(packet[i:i + 11], 2)
-                #print(f"Number mode {numb_subpackets}")
                 i += 11
                 for j in range(numb_subpackets):
                     new_packet, packet = self._parse(packet[i:])
                     subs.append(new_packet)
                     i = 0
-                    #print(f"Got: {new_packet}")
             new_packet = Operator(version, type_ID, length_type_ID, subs)
 
-        #print(f"end parse with {packet[i:]} left")
         return new_packet, packet[i:]
 
 
@@ -75,7 +65,7 @@ class Literal(Packet):
     def __str__(self):
         return f"Literal with version {self.version}, type_ID {self.type_id} and value {self.value}"
 
-    def version_sum(self):
+    def version_val(self):
         return self.version
         
     def val(self):
@@ -88,15 +78,19 @@ class Operator(Packet):
         self.length_type_ID = length_type_ID
         self.subpackets = subpackets
         
-    def version_sum(self):
-        val = self.version
-        for sub in self.subpackets:
-            val += sub.version_sum()
-        return val
+    def version_val(self):
+        return sum(sub.version_val() for sub in self.subpackets) + self.version
         
     def val(self):
-        return 0
-    
+        match self.type_id:
+            case 0: return sum(sub.val() for sub in self.subpackets)
+            case 1: return prod(sub.val() for sub in self.subpackets)
+            case 2: return min(sub.val() for sub in self.subpackets)
+            case 3: return max(sub.val() for sub in self.subpackets)
+            case 5: return self.subpackets[0].val().__gt__(self.subpackets[1].val())
+            case 6: return self.subpackets[0].val().__lt__(self.subpackets[1].val())
+            case 7: return self.subpackets[0].val().__eq__(self.subpackets[1].val())
+
     def __str__(self):
         return f"Operator with version {self.version}, type_ID {self.type_id} and {len(self.subpackets)} subpackets"
 
@@ -108,7 +102,7 @@ class Day16(Day):
         print(self.packet)
 
     def solve_part1(self):
-        return self.packet.version_sum()
+        return self.packet.version_val()
 
     def solve_part2(self):
         return self.packet.val()
